bc3f883c73666be4a521af8a47586d680b738c69
Daniel Mrozek <mrazicz@gmail.com>
2014-08-10 06:34:42 +0200

    [ADD] removed example code from lib

--- lib/web_parser.rb
+++ lib/web_parser.rb
@@ -9,34 +9,3 @@ require_relative 'web_parser/doc'
 module WebParser
 end
 
-class YahooSearch
-  # include WebParser
-  include WebParser::Doc
-
-  # define recipes
-  recipes do
-    # simplest way to define recipe
-    query          :xpath, '//input[@id="yschsp"]/@value'
-    # You can use simple normalization as last parameter
-    query_downcase :xpath, '//input[@id="yschsp"]/@value',
-                   ->(value) { value.text.downcase }
-    # You can also provide just method name for normalization
-    page_number    :css, '#pg > strong', :to_i
-    # Or you can do whatever you want to obtain value, just provide lambda as
-    # parameter
-    first_page?    :lambda, ->(doc) {
-      doc.css('#pg > strong').text.to_i == 1
-    }
-    # Nesting
-    right_links do
-      sign_in :css, '#yucs-profile', :strip
-      mail    :css, '#yucs-mail_link_id', :strip
-    end
-    # Array, usefull for example when parsing eshops
-    results :css, '#web > ol > li' do
-      name :css, '> .res h3'
-      url  :xpath, './/h3[1]/a/@href'
-    end
-  end
-end
-
